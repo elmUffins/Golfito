870d78418338d64316414fbd26d6698304c7e1fb
elmUffins <ericyes81007@gmail.com>
2024-03-30 13:33:37 -0300

    additions

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
-from pygame import *
 
 pygame.init()
 
 screen_w = 1000
-screen_h = 1000
+screen_h = 500
 
 screen = pygame.display.set_mode((screen_w, screen_h))
 pygame.display.set_caption("Golfito")
 
-# Game loop
+tile_size = 50
+
+bg_img = pygame.image.load("images/back.png")
+
+#Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    screen.blit(bg_img, (0, 0))
+
     pygame.display.flip()
 
 pygame.quit()
