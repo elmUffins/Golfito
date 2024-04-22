954faf7fbe4895f51e5d119a0c12b7a07d2d2377
elmUffins <ericyes81007@gmail.com>
2024-04-22 19:19:17 -0300

    class World():

--- main.py
+++ main.py
@@ -11,8 +11,53 @@ pygame.display.set_caption("Golfito")
 tile_size = 50
 
 bg_img = pygame.image.load("images/back.png")
+grass_img = pygame.image.load("images/grass.png")
+dirt_img = pygame.image.load("images/dirt.png")
 
-#Game loop
+# Draw Grid Function
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (0, 0, 0), (0, line * tile_size), (screen_w, line * tile_size))
+        pygame.draw.line(screen, (0, 0, 0), (line * tile_size, 0), (line * tile_size, screen_h))
+
+
+# World data and class
+world_data = \
+    [
+
+    ]
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        dirt_img = pygame.image.load("images/dirt.png")
+        grass_img = pygame.image.load("images/grass.png")
+
+        for row in range(len(data)):
+            for col in range(len(data[row])):
+                if data[row][col] == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if data[row][col] == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world = World(world_data)
+
+# Game loop
 running = True
 while running:
     for event in pygame.event.get():
@@ -20,7 +65,9 @@ while running:
             running = False
 
     screen.blit(bg_img, (0, 0))
+    world.draw()
 
+    draw_grid()
     pygame.display.flip()
 
 pygame.quit()
